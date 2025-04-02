44911bfd71cebfe13f6f7f200b9020340d849826
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-04-02 18:28:31 +0900

    差分エディタがいっぱい

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 // noinspection NpmUsedModulesInstalled
 import * as monaco from 'monaco-editor';
 
+/*
+{
+  "block.compressed_copper.machine_core": "Machine Core",
+  "container.compressed_copper.machine_core": "Machine Core",
+  "container.compressed_copper.machine_core.custom": "マシン(%s)",
+  "item.compressed_copper.compressed_copper": "Compressed Copper",
+  "item.compressed_copper.machine_cover_1": "1x Machine Cover",
+  "item.compressed_copper.test_module_1": "1x Test Module",
+  "item.compressed_copper.test_upgrade_1": "1x Test Upgrade",
+  "item_group.compressed_copper.general": "Compressed Copper"
+}
 
-const json = `{
+{
   "block.compressed_copper.machine_core": "Machine Core",
   "container.compressed_copper.machine_core": "Machine Core (No Module)",
   "container.compressed_copper.machine_core.custom": "Machine (%s)",
@@ -10,8 +21,27 @@ const json = `{
   "item.compressed_copper.test_module": "Test Module",
   "item.compressed_copper.test_upgrade": "Test Upgrade",
   "item_group.compressed_copper.general": "Compressed Copper"
-}`;
-monaco.editor.create(document.getElementById('container'), {
-  value: json,
-  language: 'json'
+}
+*/
+
+const diffEditors = [
+  "diff-container-1",
+  "diff-container-2"
+].map(containerId => {
+  return monaco.editor.createDiffEditor(
+    document.getElementById(containerId),
+    {
+      enableSplitViewResizing: true,
+      renderSideBySide: true,
+      automaticLayout: true,
+      originalEditable: true,
+    }
+  );
 });
+
+diffEditors.forEach((diff) => {
+  diff.setModel({
+    original: monaco.editor.createModel('', 'json'),
+    modified: monaco.editor.createModel('', 'json')
+  });
+})
