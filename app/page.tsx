af2210e013795aba59877e5c759f8986481475ac
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-12-09 13:44:31 +0900

    プレースホールダー

--- app/page.tsx
+++ app/page.tsx
@@ -27,10 +27,24 @@ export default function Home() {
   return (<>
     <label htmlFor="language">言語: </label>
     <select id="language">
-      <option value="json">.json</option>
+      {/*<option value="json">.json</option>*/}
       <option value="lang">.lang</option>
     </select>
-    <CustomDiffEditor />
-    <CustomDiffEditor />
+    <CustomDiffEditor onMount={editor => {
+      editor.getOriginalEditor().updateOptions({
+        placeholder: "[任意] 旧バージョンの翻訳元ファイルを貼り付けてください\n新バージョンの翻訳元ファイルと差異がある場合パッチがスキップされます",
+      });
+      editor.getModifiedEditor().updateOptions({
+        placeholder: "翻訳元ファイルを貼り付けてください",
+      });
+    }}/>
+    <CustomDiffEditor onMount={editor => {
+      editor.getOriginalEditor().updateOptions({
+        placeholder: "翻訳先ファイルを貼り付けてください",
+      });
+      editor.getModifiedEditor().updateOptions({
+        placeholder: "翻訳元ファイルに翻訳先ファイルがパッチされ表示されます",
+      });
+    }}/>
   </>);
 };
