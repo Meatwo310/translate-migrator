92018f0d81a5c18e4b8e817a8b734686e48869fd
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-12-10 04:57:00 +0900

    basePathを環境変数として指定できるように

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  basePath: "/translate-migrator",
+  basePath: process.env.BASE_PATH,
   output: "export",
 };
 
